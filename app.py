59e22d0d4219162c92953882e77d27a5581b9b16
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-09 23:57:08 -0300

    implement __name__ for easy set launch.json in vscode

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routes.users import users
 from routes.auth import auth
+import uvicorn
 app = FastAPI()
 
 app.include_router(users)
 app.include_router(auth)
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", reload=True)
