123267523db78bef56be45e20ccf4debdae40946
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-10 23:27:46 -0300

    add reviews route

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from routes.users import users
 from routes.auth import auth
+from routes.reviews import reviews
 import uvicorn
 app = FastAPI()
 
 app.include_router(users)
 app.include_router(auth)
-
+app.include_router(reviews)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True)
