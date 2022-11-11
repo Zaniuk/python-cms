87d482db6a920497df90da92fc4840bc6b081073
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-10 21:01:34 -0300

    implemet router model and schema for Users

--- models/user.py
+++ models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Table, Column
 from sqlalchemy.sql.sqltypes import Integer, String, DateTime
 from config.db import meta, engine 
 
-users = Table("users", meta,
+user = Table("users", meta,
               Column("id", Integer, primary_key=True),
               Column("name", String(255)),
               Column("email", String(255)),
