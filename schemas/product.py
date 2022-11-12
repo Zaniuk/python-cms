8c2bf532ea25e6a0f549c5593824014b3b45a16f
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-12 13:29:53 -0300

    add product schema

---
+++ schemas/product.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+class Product(BaseModel):
+    id: Optional[int]
+    name: str
+    description: str
+    buyers_id: Optional[int]
+    seller_id: int
+    created_at: str = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+    updated_at: str = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+
+    
