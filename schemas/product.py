3effd97c0ab86449ca75be9fee9e71eaf824d357
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-12 15:54:31 -0300

    add price to produc schema

--- schemas/product.py
+++ schemas/product.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class Product(BaseModel):
     name: str
     description: str
-    buyers_id: Optional[int]
     seller_id: int
+    price: int
     created_at: str = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
     updated_at: str = f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
 
