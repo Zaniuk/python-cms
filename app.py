cadf3f52d9b407d954f53317d21bf80fe7f1fbd3
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-11-12 13:28:42 -0300

    add product route

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 from routes.users import users
 from routes.auth import auth
 from routes.reviews import reviews
+from routes.products import products
 import uvicorn
 app = FastAPI()
 
 app.include_router(users)
 app.include_router(auth)
 app.include_router(reviews)
+app.include_router(products)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True)
